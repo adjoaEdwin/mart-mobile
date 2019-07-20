33fb21064a4b248a3110b82e59a856719ebc8119
AdjoaEdwin <aedwin905@gmail.com>
2019-07-20 07:36:43 +0000

    Added logout

--- src/api/constants.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCcu0NVvpaWTbt0A1BtcYXw5pF_gKTwirQ",
-  authDomain: "martmobile-53876.firebaseapp.com",
-  databaseURL: "https://martmobile-53876.firebaseio.com",
-  projectId: "martmobile-53876",
-  storageBucket: "",
-  messagingSenderId: "730378284377",
-  appId: "1:730378284377:web:8f8b5a9ee8c9fce8"
-};
